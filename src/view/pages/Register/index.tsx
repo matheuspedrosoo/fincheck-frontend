4061209bddc03d399a34876154dfd8b71ad9cc64
Matheus Pedroso <matheuspedroso404@gmail.com>
2023-09-17 19:15:12 -0300

    feat: created login and register pages

--- src/view/pages/Register/index.tsx
+++ src/view/pages/Register/index.tsx
@@ -1,7 +1,37 @@
+import { Link } from 'react-router-dom';
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+
 export function Register() {
   return (
-    <div>
-      <h1>Register Page</h1>
-    </div>
-  )
+    <>
+      <header className="flex flex-col items-center gap-4 text-center">
+        <h1 className="text-2xl font-bold text-gray-900  tracking-[-1px]">
+          Crie sua conta
+        </h1>
+
+        <p className="space-x-2">
+          <span className="text-gray-700 tracking-[-0.5px]">
+            Já possui uma conta?
+          </span>
+          <Link
+            to="/login"
+            className="tracking-[-0.5px] font-medium text-teal-900"
+          >
+            Fazer Login
+          </Link>
+        </p>
+      </header>
+
+      <form className="mt-16 flex flex-col gap-4">
+        <Input type="text" placeholder="Nome" name="email" />
+        <Input type="email" placeholder="E-mail" name="name" />
+        <Input type="password" placeholder="Senha" name="password" />
+
+        <Button type="submit" className="mt-2">
+          Criar conta
+        </Button>
+      </form>
+    </>
+  );
 }
