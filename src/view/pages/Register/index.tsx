d7708f4391440844eb8f7c81db37473d71c5af40
Matheus Pedroso <matheuspedroso404@gmail.com>
2023-09-18 00:02:00 -0300

    feat: created spinner on loading

--- src/view/pages/Register/index.tsx
+++ src/view/pages/Register/index.tsx
@@ -4,7 +4,7 @@ import { Button } from '../../components/Button';
 import { useRegisterController } from './useRegisterController';
 
 export function Register() {
-  const { handleSubmit, register, errors } = useRegisterController();
+  const { handleSubmit, register, errors, isLoading } = useRegisterController();
 
   return (
     <>
@@ -46,7 +46,7 @@ export function Register() {
           error={errors.password?.message}
         />
 
-        <Button type="submit" className="mt-2">
+        <Button type="submit" className="mt-2" isLoading={isLoading}>
           Criar conta
         </Button>
       </form>
