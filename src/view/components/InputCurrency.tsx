23a41fa1fd6cce7986c37c0a2d9923b246419836
Matheus Pedroso <matheuspedroso404@gmail.com>
2023-10-14 17:11:05 -0300

    feat: imlements validation on input

--- src/view/components/InputCurrency.tsx
+++ src/view/components/InputCurrency.tsx
@@ -1,12 +1,34 @@
+import { CrossCircledIcon } from '@radix-ui/react-icons';
 import { NumericFormat } from 'react-number-format';
+import { cn } from '../../app/utils/cn';
 
-export function InputCurrency() {
+interface InputCurrencyProps {
+  error?: string;
+  value?: string | number;
+  onChange?(value: string): void;
+}
+
+export function InputCurrency({ error, onChange, value }: InputCurrencyProps) {
   return (
-    <NumericFormat
-      thousandSeparator="."
-      decimalSeparator=","
-      className="w-full text-gray-800 text-[32px] font-bold tracking-[-1px] outline-none"
-      defaultValue="0"
-    />
+    <div>
+      <NumericFormat
+        thousandSeparator="."
+        decimalSeparator=","
+        onChange={(event) => onChange?.(event.target.value)}
+        value={value}
+        className={cn(
+          'w-full text-gray-800 text-[32px] font-bold tracking-[-1px] outline-none',
+          error && 'text-red-900'
+        )}
+        defaultValue="0"
+      />
+
+      {error && (
+        <div className="flex items-center gap-2 mt-2 text-red-900">
+          <CrossCircledIcon />
+          <span className="text-red-900 text-xs">{error}</span>
+        </div>
+      )}
+    </div>
   );
 }
